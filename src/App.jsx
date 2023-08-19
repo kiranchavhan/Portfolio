f42aadcc7c60d5ecb3d14ac1d80e51efb32a7944
KiranChavhan <chavhankiran123@gmail.com>
2023-08-19 22:33:39 +0530

    check-point component intial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Checkpoint from './milestone/checkpoint';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkpoint title={"HighSchool Education"} description={"I did my High school from APS kirkee"}/>
+    </div>
+  );
+}
+
+export default App;
