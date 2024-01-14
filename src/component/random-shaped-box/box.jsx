40f79450c61833c932d1838ee204baff5f587ca6
Kiran Chavhan <kiranchavhan@Kirans-MacBook-Air.local>
2024-01-14 07:19:18 +0530

    intro-changes-profile-pic

--- src/component/random-shaped-box/box.jsx
+++ src/component/random-shaped-box/box.jsx
@@ -2,37 +2,37 @@ import React from "react";
 import './box.css'
 import Typewriter from "typewriter-effect";
 
+import kiraa from '../../media/images/kiraa.png'
+
 const data = ['Software Engineer','FullStack Developer','MERN Stack Developer','Frontend Developer','Backend Developer'];
 
-
 const Box = () => {
-  return (
-    <>
-      <div className='box'>
-      <div className='circle'/>
-        <div className='intro'>
-          <h2>Hi,I am</h2>
-          <h1>Kiran Chavhan</h1>
-          <Typewriter
-            className='description'
-            options={{
-              cursor: ''
-            }}
-            onInit={typewriter => {
-              let i = 0;
-              setInterval(() => {
-                typewriter
-                  .deleteAll()
-                  .typeString(data[i++ % data.length])
-                  .start();
-              }, 500);
-            }}
-          />
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='box'>
+                <img src={kiraa} alt='profile' className='profile'/>
+                <div className='intro'>
+                    <h2>Hi, I am</h2>
+                    <h1>Kiran Chavhan</h1>
+                    <Typewriter
+                        className='description'
+                        options={{
+                            cursor: ''
+                        }}
+                        onInit={typewriter => {
+                            let i = 0;
+                            setInterval(() => {
+                                typewriter
+                                    .deleteAll()
+                                    .typeString(data[i++ % data.length])
+                                    .start();
+                            }, 500);
+                        }}
+                    />
+                </div>
+            </div>
+        </>
+    )
 }
 
-//use copilot to
 export default Box;
